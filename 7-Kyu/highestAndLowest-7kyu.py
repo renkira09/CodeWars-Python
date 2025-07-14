9ac8f74cd1c77a530bb8ed47b9e9b534d022c82d
ChinchillaBiiru09 <razha0075@gmail.com>
2025-07-15 05:45:59 +0700

    15.07.25 @ 7kyu - highest and lowest [edit]

--- 7-Kyu/highestAndLowest-7kyu.py
+++ 7-Kyu/highestAndLowest-7kyu.py
@@ -42,6 +42,6 @@ def high_and_low(numbers):
 
 # high_and_low("1 2 3 4 5")
 print(high_and_low("1 2 3 4 5"))
-# print(high_and_low("1 2 -3 4 5"))
-# print(high_and_low("1 9 3 4 -5"))
-# print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+print(high_and_low("1 2 -3 4 5"))
+print(high_and_low("1 9 3 4 -5"))
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
